1117b52237f20cfa2d676fe9689b36ab2b8f460c
goliatone <burgosemi@gmail.com>
2017-06-27 17:33:18 -0400

    Rename template main file to index.js

--- templates/lib/#name#.js
+++
@@ -1,9 +0,0 @@
-/**
- * %{git.name}%
- * %{date}%
- * This is a file generated with love :)
- */
-
-module.exports = function %{name}%(){
-    console.log('We are running %{name}%');
-};
---
+++ templates/lib/index.js
@@ -0,0 +1,9 @@
+/**
+ * %{git.name}%
+ * %{date}%
+ * This is a file generated with love :)
+ */
+
+module.exports = function %{moduleName}%(){
+    console.log('We are running %{moduleName}%');
+};
