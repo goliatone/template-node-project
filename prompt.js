7e39772158a11742b66b60ea800ccebd9d3bc37a
goliatone <burgosemi@gmail.com>
2017-06-27 17:30:47 -0400

    Collect nice name

--- prompt.js
+++ prompt.js
@@ -1,10 +1,28 @@
 'use strict';
+const slug = require('slug');
+
+slug.defaults.mode ='rfc3986';
+slug.defaults.modes['rfc3986'] = {
+    replacement: '-',      // replace spaces with replacement
+    symbols: true,         // replace unicode symbols or not
+    remove: null,          // (optional) regex to remove characters
+    lower: true,           // result in lower case
+    charmap: slug.charmap, // replace special characters
+    multicharmap: slug.multicharmap // replace multi-characters
+};
 
 module.exports = [
     {
         type: 'input',
         name: 'name',
-        message: 'What\'s the project\'s name'
+        message: 'What\'s the project\'s name?'
+    },{
+        type: 'input',
+        name: 'moduleName',
+        default: function(answer) {
+            return slug(answer.name);
+        },
+        message: 'What\'s the module\'s name? This will be used for the repo URL'
     },
     {
         type: 'input',
