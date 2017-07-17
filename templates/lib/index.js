4d8d1bbde9e05d6d12598b0763353ddf20b6dd6b
goliatone <burgosemi@gmail.com>
2017-07-16 21:48:26 -0400

    Fix missing quotes

--- templates/lib/index.js
+++ templates/lib/index.js
@@ -3,7 +3,7 @@
  * %{date}%
  * This is a file generated with love :)
  */
-module.exports[%{moduleName}%] = function(){
+module.exports['%{moduleName}%'] = function(){
     console.log('We are running %{moduleName}%');
     return true;
 };
